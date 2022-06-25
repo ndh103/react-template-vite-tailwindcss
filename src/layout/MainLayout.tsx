9a56aafa7df5dee449a4bdc908a1dfb3b76af534
Nguyen Danh Ha <ndh103@gmail.com>
2022-06-25 16:41:18 +0700

    add alias and render svg

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { PropsWithChildren } from 'react'
+import { ReactComponent as MenuIcon } from '@/assets/images/menu.svg'
 
 function MainLayout({ children }: PropsWithChildren): JSX.Element {
   return (
     <div>
-      <div className="h-12 w-full bg-teal-400 fixed p-3">Site header</div>
+      <div className="h-12 w-full bg-teal-400 fixed p-3">
+        <MenuIcon className="h-4 w-4 inline-block" />
+        Site header
+      </div>
       <div className="flex pt-12">
         <div className="flex-none overflow-auto h-full fixed w-80 bg-gray-100 p-3">
           Left sidebar
